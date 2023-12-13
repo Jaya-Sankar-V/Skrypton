cbd33de3ec8f19348b2c85eac562f73d8e7ff4c4
Jaya-Sankar-V <153719783+Jaya-Sankar-V@users.noreply.github.com>
2023-12-13 14:48:55 +0530

    Add files via upload

---
+++ one.py
@@ -0,0 +1,16 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+#duplicates
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+#length
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+# all data types
+
+list1 = ["abc", 34, True, 40, "male"]
